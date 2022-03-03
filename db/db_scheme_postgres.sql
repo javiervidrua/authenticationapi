8d0190c5f317378c0defd8fbd4284fca6e18ea63
javiervidrua <67796321+javiervidrua@users.noreply.github.com>
2022-03-03 08:53:54 +0100

    Created the database schema in DBML and exported it to PostgreSQL

---
+++ db/db_scheme_postgres.sql
@@ -0,0 +1,35 @@
+CREATE TYPE "roles" AS ENUM (
+  'regular',
+  'admin'
+);
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "role" roles DEFAULT 'regular',
+  "verified" boolean DEFAULT false,
+  "created" timestamp DEFAULT (now()),
+  "last" timestamp,
+  "email" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "address" varchar,
+  "city" varchar,
+  "postal" varchar,
+  "phone" varchar
+);
+
+CREATE TABLE "users_groups" (
+  "user_id" int,
+  "group_id" int,
+  PRIMARY KEY ("user_id", "group_id")
+);
+
+CREATE TABLE "groups" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar NOT NULL
+);
+
+ALTER TABLE "users_groups" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "users_groups" ADD FOREIGN KEY ("group_id") REFERENCES "groups" ("id");
+
